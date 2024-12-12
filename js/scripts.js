6648baeabb6987ca5b4d86fc55414e75041b0096
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2024-12-12 22:48:32 +0100

    created array and then added pokemon
    empty array created.  pokemon then added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 let pokemonList = [];
+pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'], HP: 45, Attack: 49 },
+    {name: 'Charmander', height: 0.6, type: ['Fire'], HP: 39, Attack: 52 },
+    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying'], HP: 78, Attack: 84 },
+    {name: 'Squirtle', height: 0.5, type: ['Water'], HP: 44, Attack: 48 },
+    {name: 'Blastoise', height: 1.6, type: ['Water'], HP: 79, Attack: 83 },
+    {name: 'Pidgey', height: 0.3, type: ['Flying', 'Normal'], HP: 40, Attack: 45 },
+    {name: 'Rattata', height: 0.3, type: ['Normal'], HP: 30, Attack: 56 },
+    {name: 'Pikachu', height: 0.4, type: ['Electric'], HP: 35, Attack: 55 }
+];
