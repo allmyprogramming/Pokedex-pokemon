beda4fd287374afe0b90d3a967ab21bbd19d8a44
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2024-12-25 19:02:28 +0100

    created pokemonRepository with getAll() functiion

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-let pokemonList = [];
-pokemonList = [
+let pokemonRepository = (function () 
+    {
+  pokemonList = [
     { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'], HP: 45, Attack: 49 },
     { name: 'Charmander', height: 0.6, type: ['Fire'], HP: 39, Attack: 52 },
     { name: 'Charizard', height: 1.7, type: ['Fire', 'Flying'], HP: 78, Attack: 84 },
@@ -8,58 +9,7 @@ pokemonList = [
     { name: 'Pidgey', height: 0.3, type: ['Flying', 'Normal'], HP: 40, Attack: 45 },
     { name: 'Rattata', height: 0.3, type: ['Normal'], HP: 30, Attack: 56 },
     { name: 'Pikachu', height: 0.4, type: ['Electric'], HP: 35, Attack: 55 }
-];
-//pokemonlist array
-
-/*function(pokemon) allows each element of pokemonList to the forEach function*/
-
-
-pokemonList.forEach(function(pokemon){
-
-
-  let height = pokemon.height;
-  let name = pokemon.name;
-  let type = pokemon.type;
-  
-
-  if (height > 1.6) {
-    document.write(
-        `Name: ${name}, Height: ${height}. 
-        Wow! That's a big Pokémon. Type: ${type}<br>`
-    );
-} else {
-    document.write(
-        `Name: ${name}, Height: ${height}, Type: ${type}<br>`
-    );
-}
-})
-
-/*
-let height = pokemonList[i].height;
-let name = pokemonList[i].name;
-let type = pokemonList[i].type;
-
-    if (height > 1.6) {
-        document.write(
-            `Name: ${name}, Height: ${height}. Wow! That's a big Pokémon. Type: ${type}<br>`
-        );
-    } else {
-        document.write(
-            `Name: ${name}, Height: ${height}, Type: ${type}<br>`
-        );
-    }
-}
-
-*/
-
-
-// loops through list and checks if pokemon is bigger that 1.6 meters and add
-// wow thats a big Pokemon next to height.
-
-/*
-let pokemonRepository = (function () {
-    let pokemonList = [];
-  
+];  
     function add(pokemon) {
       pokemonList.push(pokemon);
     }
@@ -72,5 +22,25 @@ let pokemonRepository = (function () {
       add: add,
       getAll: getAll
     };
-  })();
-  */
+    })();
+  
+
+/*function(pokemon) allows each element of pokemonList to the forEach function*/
+
+pokemonRepository.getAll().forEach(function(pokemon){
+
+  let height = pokemon.height;
+  let name = pokemon.name;
+  let type = pokemon.type;  
+
+  if (height > 1.6) {
+    document.write(
+        `Name: ${name}, Height: ${height}.\u00A0\u00A0Wow! That's a big Pokémon. Type: ${type}<br>`
+    );
+} else {
+    document.write(
+        `Name: ${name}, Height: ${height}, Type: ${type}<br>`
+    );
+}
+})
+/*\u00A0 is a Unicode escape sequence that represents a non-breaking space*/
