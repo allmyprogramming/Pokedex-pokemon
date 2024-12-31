4d68be607e71910ffa8643ca1a84917298813a61
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2024-12-31 17:49:53 +0100

    amended comments

--- js/scripts.js
+++ js/scripts.js
@@ -64,7 +64,7 @@ let pokemonRepository = (function () {
     return fetch(url).then(function (response) {
       return response.json();
     }).then(function (details) {
-      // Now we add the details to the item
+      // added the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types.map((typeInfo) => typeInfo.type.name);  // Extracting the type names
